504070fb5592fa0e662e7f5c725266ead13dfb92
Kiril Isakov <kirix.isakov@gmail.com>
2023-02-02 17:34:08 +0100

    Apply code format

--- whatever_disentangler/whatever_disentangler.py
+++ whatever_disentangler/whatever_disentangler.py
@@ -1,6 +1,7 @@
 import colorama
 import requests
-import os, sys
+import os
+import sys
 import time
 import types
 from urllib.parse import urlencode, unquote
@@ -19,9 +20,11 @@ class Disentangler:
         self.expected_str = None
         self.recursivity_depth = 1
 
-    def disentangle(self, *, str_to_fix: str, encoding_from=None, encoding_to=None, expected_str: str=None, recursivity_depth: int=1) -> types.GeneratorType:
+    def disentangle(self, *, str_to_fix: str, encoding_from=None, encoding_to=None, expected_str: str = None, recursivity_depth: int = 1) -> types.GeneratorType:
         if str_to_fix is None or str_to_fix.strip() == '':
-            raise ValueError(f"The required parameter str_to_fix (string to fix) is empty.")
+            raise ValueError(
+                f"The required parameter str_to_fix (string to fix) is empty."
+            )
 
         def _fix(str_to_fix: str, encoding_from: str, encoding_to: str):
             return str_to_fix.encode(encoding_from).decode(encoding_to, errors='replace')
@@ -36,6 +39,7 @@ class Disentangler:
                 return standard_encodings
 
         cache = []
+
         def _fix_legacy_encoding(str_to_fix: str, _encoding_from: list, _encoding_to: list, expected_str: str, recursivity_depth: int):
             for enc_from in _encoding_from:
                 for enc_to in _encoding_to:
@@ -56,9 +60,9 @@ class Disentangler:
                             "recursivity_depth": recursivity_depth
                         }
                         if (expected_str is None
-                        or expected_str.strip() == ''
-                        or expected_str.strip().lower() == fixed_str.strip().lower()
-                        or recursivity_depth > 1):
+                            or expected_str.strip() == ''
+                            or expected_str.strip().lower() == fixed_str.strip().lower()
+                                or recursivity_depth > 1):
                             if return_pack not in cache:
                                 cache.append(return_pack)
                                 yield return_pack
@@ -71,14 +75,17 @@ class Disentangler:
 
         yield from _fix_legacy_encoding(str_to_fix, _resolve_encodings(encoding_from), _resolve_encodings(encoding_to), expected_str, recursivity_depth)
 
-
     def flatten_legibly(self, result_generator: types.GeneratorType) -> None:
         for d in result_generator:
-            indent_width = 4 * (self.recursivity_depth - int(d['recursivity_depth']))
+            indent_width = 4 * (
+                self.recursivity_depth - int(d['recursivity_depth'])
+            )
             indent = indent_width * ' '
             indent += '-> ' if indent_width > 0 else ''
             match = self.expected_str is not None and d['fixed_str'] == self.expected_str
-            fixed_str = d['fixed_str'] if not match else colorama.Fore.GREEN + d['fixed_str'] + colorama.Style.RESET_ALL
+            fixed_str = d['fixed_str']
+            if match:
+                fixed_str = colorama.Fore.GREEN + fixed_str + colorama.Style.RESET_ALL
             s = f"{indent}{d['str_to_fix']!r} ({d['encoding_from']!r}) -> '{fixed_str}' ({d['encoding_to']!r})"
             print(s)
             if match:
@@ -91,30 +98,35 @@ class RemoteDisentangler:
         self.expected_str = None
         self.recursivity_depth = 1
 
-
-    async def fetch_response(self, *, str_to_fix: str, encoding_from=None, encoding_to=None, expected_str: str=None, recursivity_depth: int=1):
+    async def fetch_response(self, *, str_to_fix: str, encoding_from=None, encoding_to=None, expected_str: str = None, recursivity_depth: int = 1):
         self.expected_str = expected_str
         self.recursivity_depth = recursivity_depth
         qparams = {}
-        params = [str_to_fix, encoding_from, encoding_to, expected_str, recursivity_depth]
-        param_names = ['str_to_fix', 'encoding_from', 'encoding_to', 'expected_str', 'recursivity_depth']
+        params = [str_to_fix, encoding_from,
+                  encoding_to, expected_str, recursivity_depth]
+        param_names = ['str_to_fix', 'encoding_from',
+                       'encoding_to', 'expected_str', 'recursivity_depth']
         for pname, pvalue in zip(param_names, params):
             if pvalue:
                 qparams[pname] = pvalue
         response = requests.get(self.endpoint + '?' + urlencode(qparams))
         return response
 
-
     def flatten_legibly(self, response_obj) -> None:
         payload, req_url = (response_obj.json(), response_obj.request.url)
         if payload in (None, []):
-            print(f"Got an empty response {payload!r} ({type(payload).__name__}) for the request {unquote(req_url)!r}")
+            print(
+                f"Got an empty response {payload!r} ({type(payload).__name__}) for the request {unquote(req_url)!r}")
             return
         for d in payload:
-            indent_width = 4 * (self.recursivity_depth - int(d['recursivity_depth']))
+            indent_width = 4 * (
+                self.recursivity_depth - int(d['recursivity_depth'])
+            )
             indent = indent_width * ' '
             indent += '-> ' if indent_width > 0 else ''
             match = self.expected_str is not None and d['fixed_str'] == self.expected_str
-            fixed_str = d['fixed_str'] if not match else colorama.Fore.GREEN + d['fixed_str'] + colorama.Style.RESET_ALL
+            fixed_str = d['fixed_str']
+            if match:
+                fixed_str = colorama.Fore.GREEN + fixed_str + colorama.Style.RESET_ALL
             s = f"{indent}{d['str_to_fix']!r} ({d['encoding_from']!r}) -> '{fixed_str}' ({d['encoding_to']!r})"
             print(s)
