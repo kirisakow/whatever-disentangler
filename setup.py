11482e8337f773b39d4d68bc4baf2a3e83ecc42f
Kiril Isakov <kirix.isakov@gmail.com>
2024-05-30 10:28:35 +0200

    update dependencies

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='whatever_detangler',
-    version='0.0.1',
+    name='whatever_disentangler',
+    version='0.1.0',
     author='Kiril Isakov',
     author_email='kirix.isakov@gmail.com',
     url='https://github.com/kirisakow/whatever_disentangler',
-    description='brute-force disentangler for legacy encodings',
+    description='A brute-force disentangler for legacy encodings',
     packages=find_packages(),
     install_requires=[],
     python_requires='>=3.9'
